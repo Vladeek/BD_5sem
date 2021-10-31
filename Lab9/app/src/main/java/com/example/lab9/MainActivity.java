86e3215796c481408a7d5a4d2c8a0f5b2ed6d1e5
Vladeek <vlad.berinchikoff@gmail.com>
2021-10-31 15:14:34 +0300

    Add try/catch to methods

--- Lab9/app/src/main/java/com/example/lab9/MainActivity.java
+++ Lab9/app/src/main/java/com/example/lab9/MainActivity.java
@@ -34,87 +34,100 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void insert(View view) {
-        int idStr = Integer.parseInt(ID.getText().toString());
-        float fStr = Float.parseFloat(F.getText().toString());
-        String tStr = T.getText().toString();
-        ContentValues values = new ContentValues();
-        values.put("ID", idStr);
-        values.put("F", fStr);
-        values.put("T", tStr);
-        // вставляем, возвращает primary key
-        long newRowId;
-        newRowId = db.insert(
-                DatabaseHelper.TABLE,
-                null,
-                values);
+        try {
+            int idStr = Integer.parseInt(ID.getText().toString());
+            float fStr = Float.parseFloat(F.getText().toString());
+            String tStr = T.getText().toString();
+            ContentValues values = new ContentValues();
+            values.put("ID", idStr);
+            values.put("F", fStr);
+            values.put("T", tStr);
+            // вставляем, возвращает primary key
+            long newRowId;
+            newRowId = db.insert(
+                    DatabaseHelper.TABLE,
+                    null,
+                    values);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void select(View view) {
-        String[] projection = {DatabaseHelper.COLUMN_ID, DatabaseHelper.COLUMN_F, DatabaseHelper.COLUMN_T};
-        String selection = DatabaseHelper.COLUMN_ID + " = ?";
-        String[] selectionArgs = {ID.getText().toString()};
-
-        Cursor cursor = db.query(
-                DatabaseHelper.TABLE, // имя таблицы
-                projection, // столбцы
-                selection, // столбцы для WHERE
-                selectionArgs, // значения для WHERE
-                null, // не группировать строки
-                null, // не фильтровать
-                null // порядок сортировки
-        );
-
-        int count = cursor.getCount();
-
-        cursor.moveToFirst();
-        String idValue = cursor.getString(0);
-        float fValue = cursor.getFloat(1);
-        String tValue = cursor.getString(2);
-        ID.setText(idValue);
-        F.setText(String.valueOf(fValue));
-        T.setText(tValue);
-
-        cursor.close();
-
+        try {
+            String[] projection = {DatabaseHelper.COLUMN_ID, DatabaseHelper.COLUMN_F, DatabaseHelper.COLUMN_T};
+            String selection = DatabaseHelper.COLUMN_ID + " = ?";
+            String[] selectionArgs = {ID.getText().toString()};
+            Cursor cursor = db.query(
+                    DatabaseHelper.TABLE, // имя таблицы
+                    projection, // столбцы
+                    selection, // столбцы для WHERE
+                    selectionArgs, // значения для WHERE
+                    null, // не группировать строки
+                    null, // не фильтровать
+                    null // порядок сортировки
+            );
+            int count = cursor.getCount();
+            cursor.moveToFirst();
+            String idValue = cursor.getString(0);
+            float fValue = cursor.getFloat(1);
+            String tValue = cursor.getString(2);
+            ID.setText(idValue);
+            F.setText(String.valueOf(fValue));
+            T.setText(tValue);
+            cursor.close();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+        }
 
     }
 
     public void selectRaw(View view) {
-        String[] selectionArgs = {ID.getText().toString()};
-        Cursor cursor = db.rawQuery("select ID, F, T from SimpleTable where ID = ?", selectionArgs);
-        cursor.moveToFirst();
-        String idValue = cursor.getString(0);
-        float fValue = cursor.getFloat(1);
-        String tValue = cursor.getString(2);
-        ID.setText(idValue);
-        F.setText(String.valueOf(fValue));
-        T.setText(tValue);
-
-        cursor.close();
+        try {
+            String[] selectionArgs = {ID.getText().toString()};
+            Cursor cursor = db.rawQuery("select ID, F, T from SimpleTable where ID = ?", selectionArgs);
+            cursor.moveToFirst();
+            String idValue = cursor.getString(0);
+            float fValue = cursor.getFloat(1);
+            String tValue = cursor.getString(2);
+            ID.setText(idValue);
+            F.setText(String.valueOf(fValue));
+            T.setText(tValue);
+            cursor.close();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void update(View view) {
-        String[] selectionArgs = {ID.getText().toString()};
-        String selection = DatabaseHelper.COLUMN_ID + " LIKE ?";
-        int idStr = Integer.parseInt(ID.getText().toString());
-        float fStr = Float.parseFloat(F.getText().toString());
-        String tStr = T.getText().toString();
-        ContentValues values = new ContentValues();
-        values.put("F", fStr);
-        values.put("T", tStr);
-        // вставляем, возвращает primary key
-        long newRowId;
-        newRowId = db.update(
-                DatabaseHelper.TABLE,
-                values,
-                selection,
-                selectionArgs);
+        try {
+            String[] selectionArgs = {ID.getText().toString()};
+            String selection = DatabaseHelper.COLUMN_ID + " LIKE ?";
+            int idStr = Integer.parseInt(ID.getText().toString());
+            float fStr = Float.parseFloat(F.getText().toString());
+            String tStr = T.getText().toString();
+            ContentValues values = new ContentValues();
+            values.put("F", fStr);
+            values.put("T", tStr);
+            long newRowId;
+            newRowId = db.update(
+                    DatabaseHelper.TABLE,
+                    values,
+                    selection,
+                    selectionArgs);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void delete(View view) {
-        String selection = DatabaseHelper.COLUMN_ID + " LIKE ?";
-        String[] selectionArgs = {ID.getText().toString()};
-        int deletedRows = db.delete(DatabaseHelper.TABLE, selection,
-                selectionArgs);
+        try {
+            String selection = DatabaseHelper.COLUMN_ID + " LIKE ?";
+            String[] selectionArgs = {ID.getText().toString()};
+            int deletedRows = db.delete(DatabaseHelper.TABLE, selection,
+                    selectionArgs);
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_LONG).show();
+        }
     }
 }
